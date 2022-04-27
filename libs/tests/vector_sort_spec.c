3dc892914fb498a7c01537915bdd8a4b0018a8b5
Nazareno Montesoro <n.montesoro@gmail.com>
2022-04-27 16:53:48 -0300

    Added vector_sort_selection

--- libs/tests/vector_sort_spec.c
+++ libs/tests/vector_sort_spec.c
@@ -24,6 +24,24 @@ int cmp_func(void *a_ptr, void *b_ptr)
     return r;
 }
 
+int cmp_func_sel(void **list, int length)
+{
+    int min = *(int *) list[0];
+    int index = 0;
+
+    for (int i = 1; i < length; i++)
+    {
+        int val = *(int *) list[i];
+        if (val < min)
+        {
+            min = val;
+            index = i;
+        }
+    }
+    
+    return index;
+}
+
 spec("vector_sort")
 {
     vector *v = NULL;
@@ -135,6 +153,69 @@ spec("vector_sort")
             it("should have sorted the values")
             {
                 vector *v_sorted = vector_sort_bubble(v, sizeof(int), &cmp_func);
+                
+                vector_free(v);
+                v = v_sorted;
+
+                int correct_vals[] = {-9, -7, -5, -3, -1, 0, 2, 4, 6, 8};
+
+                for (int i = 0; i < 10; i++)
+                {
+                    stored_val = *(int *)vector_get(v, i);
+
+                    check(stored_val == correct_vals[i],
+                          "Incorrect value at index %i", i);
+                }
+
+                vector_free(v_sorted);
+            }
+        }
+    }
+
+        describe("vector_sort_selection")
+    {
+
+        context("when v is NULL")
+        {
+            it("should return NULL")
+            {
+                check(!vector_sort_selection(NULL, 4, &cmp_func));
+            }
+        }
+
+        context("when comparison function pointer is NULL")
+        {
+            it("should return NULL")
+            {
+                check(!vector_sort_selection(v, 4, NULL));
+            }
+        }
+
+        context("when data_size is invalid")
+        {
+            it("should return NULL")
+            {
+                check(!vector_sort_selection(v, -1, &cmp_func));
+                check(!vector_sort_selection(v, 0, &cmp_func));
+            }
+        }
+
+        context("when everything is right")
+        {
+            v = vector_new(10);
+            int stored_val = 0;
+
+            /* [0, -1, 2, -3, 4, -5, 6, -7, 8, -9] */
+            for (int i = 0; i < 10; i++)
+            {
+                int *val = (int *)malloc(sizeof(int));
+                *val = pow(-1, i) * i;
+                vector_add(v, (void *)val);
+            }
+
+            it("should have sorted the values")
+            {
+                vector *v_sorted = vector_sort_selection(v, sizeof(int), &cmp_func_sel);
                 
                 vector_free(v);
                 v = v_sorted;
