78521e4c30eb3fcdb425a0a1961a3cc93d7a50bc
Nazareno Montesoro <n.montesoro@gmail.com>
2022-04-27 16:10:23 -0300

    Add complex numbers

--- TP2/5.c
+++ TP2/5.c
@@ -3,6 +3,52 @@
 #include "../libs/guia_2.h"
 #include "../libs/vector.h"
 #include "../libs/myFractions.h"
+#include "../libs/myComplex.h"
+
+int compare_fractions(void *a, void *b);
+void print_fractions(void *datum);
+void test_fractions(void);
+int compare_cpx_real(void *a, void *b);
+void print_complex_numbers(void *datum);
+void test_complex_real(void);
+void test_complex_img(void);
+int compare_cpx_img(void *a, void *b);
+
+int compare_cpx_real(void *a, void *b)
+{
+    double real_a = complex_real((complex_t *)a),
+           real_b = complex_real((complex_t *)b);
+    int result = 0;
+
+    if (real_a > real_b)
+    {
+        result = 1;
+    }
+    else if (real_a < real_b)
+    {
+        result = -1;
+    }
+
+    return result;
+}
+
+int compare_cpx_img(void *a, void *b)
+{
+    double img_a = complex_imaginary((complex_t *)a),
+           img_b = complex_imaginary((complex_t *)b);
+    int result = 0;
+
+    if (img_a > img_b)
+    {
+        result = 1;
+    }
+    else if (img_a < img_b)
+    {
+        result = -1;
+    }
+
+    return result;
+}
 
 int compare_fractions(void *a, void *b)
 {
@@ -32,10 +78,30 @@ void print_fractions(void *datum)
     char str[32] = "";
 
     sprintf(str, "%i/%i", num, den);
-    printf("%15s", str);
+    printf("%20s", str);
 }
 
+void print_complex_numbers(void *datum)
+{
+    char str[32] = "";
+    complex_t *cpx = (complex_t *)datum;
+
+    sprintf(str, "%3.2f%+3.2fi", complex_real(cpx), complex_imaginary(cpx));
+    printf("%20s", str);
+}
+
+
+
 int main(int argc, char const *argv[])
+{
+    test_fractions();
+    test_complex_real();
+    test_complex_img();
+
+    return 0;
+}
+
+void test_fractions(void)
 {
     vector *v = vector_new(5),
            *v_sorted = (vector *)NULL;
@@ -50,6 +116,38 @@ int main(int argc, char const *argv[])
 
     vector_free(v);
     vector_free(v_sorted);
+}
 
-    return 0;
+void test_complex_real(void)
+{
+    vector *v = vector_new(5),
+           *v_sorted = (vector *)NULL;
+
+    fill_vector_with_random_cpx_numbers(v, 5);
+    v_sorted = vector_sort_insertion(v, sizeof(complex_t), &compare_cpx_real);
+
+    vector_print(v, &print_complex_numbers);
+    printf("\n");
+    vector_print(v_sorted, &print_complex_numbers);
+    printf("\n");
+
+    vector_free(v);
+    vector_free(v_sorted);
+}
+
+void test_complex_img(void)
+{
+    vector *v = vector_new(5),
+           *v_sorted = (vector *)NULL;
+
+    fill_vector_with_random_cpx_numbers(v, 5);
+    v_sorted = vector_sort_insertion(v, sizeof(complex_t), &compare_cpx_img);
+
+    vector_print(v, &print_complex_numbers);
+    printf("\n");
+    vector_print(v_sorted, &print_complex_numbers);
+    printf("\n");
+
+    vector_free(v);
+    vector_free(v_sorted);
 }
