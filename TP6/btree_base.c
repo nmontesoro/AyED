89d8434cb7beb21f0598027dc8d6d5872a51f71e
Nazareno Montesoro <n.montesoro@gmail.com>
2022-05-20 14:42:25 -0300

    Initial commit

---
+++ TP6/btree_base.c
@@ -0,0 +1,279 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "btree_base.h"
+
+btn *btn_new(t_elem_btree value)
+{
+    btn *node = malloc(sizeof(btn));
+
+    if (node)
+    {
+        node->value = value;
+        node->left = NULL;
+        node->right = NULL;
+    }
+
+    return node;
+}
+
+void btn_free(btn **node)
+{
+    if (node)
+    {
+        if (*node)
+        {
+            btn_free(&((*node)->left));
+            btn_free(&((*node)->right));
+            free(*node);
+            *node = NULL;
+        }
+    }
+}
+
+void btn_free_helper(btn *node, void *ctx)
+{
+}
+
+btn **btn_find(btn **node, t_elem_btree value, int cmp(t_elem_btree, t_elem_btree))
+{
+    btn **result = NULL;
+
+    if (node && *node)
+    {
+        if (!cmp((*node)->value, value))
+        {
+            result = node;
+        }
+        else
+        {
+            result = btn_find(&((*node)->left), value, cmp);
+            if (!result)
+            {
+                result = btn_find(&((*node)->right), value, cmp);
+            }
+        }
+    }
+
+    return result;
+}
+
+int btn_isLeaf(btn *node)
+{
+    int result = 0;
+
+    if (node)
+    {
+        result = !(node->left || node->right);
+    }
+
+    return result;
+}
+
+int btn_count(btn *node)
+{
+    int result = 0;
+    if (node != NULL)
+    {
+        result = 1 + btn_count(node->left) + btn_count(node->right);
+    }
+    return result;
+}
+
+int btn_insert(btn **node, btn *newNode)
+{
+    int result = 0,
+        lcnt = 0,
+        rcnt = 0;
+
+    if (node)
+    {
+        if (*node)
+        {
+            if (!((*node)->left))
+            {
+                (*node)->left = newNode;
+            }
+            else if (!((*node)->right))
+            {
+                (*node)->right = newNode;
+            }
+            else
+            {
+                lcnt = btn_count((*node)->left);
+                rcnt = btn_count((*node)->right);
+
+                if (lcnt <= rcnt)
+                {
+                    btn_insert(&((*node)->left), newNode);
+                }
+                else if (rcnt < lcnt)
+                {
+                    btn_insert(&((*node)->right), newNode);
+                }
+            }
+        }
+        else
+        {
+            *node = newNode;
+        }
+    }
+
+    return result;
+}
+
+int btn_insert_value(btn **node, t_elem_btree value)
+{
+    int result = 0;
+    btn *new_node = NULL;
+
+    if (node)
+    {
+        new_node = btn_new(value);
+
+        result = btn_insert(node, new_node);
+    }
+
+    return result;
+}
+
+int _btn_level(btn *node, t_elem_btree value, int level, int cmp(t_elem_btree, t_elem_btree))
+{
+    int result = 0;
+
+    if (node)
+    {
+        if (!cmp(node->value, value))
+        {
+            result = level;
+        }
+        else
+        {
+            result = _btn_level(node->left, value, level + 1, cmp);
+
+            if (!result)
+            {
+                result = _btn_level(node->right, value, level + 1, cmp);
+            }
+        }
+    }
+
+    return result;
+}
+
+int btn_level(btn *root, t_elem_btree value, int cmp(t_elem_btree, t_elem_btree))
+{
+    return _btn_level(root, value, 1, cmp);
+}
+
+int _max(int a, int b) { return (a > b) ? a : b; }
+
+int _btn_height(btn *node, int height)
+{
+    int result = height;
+
+    if (node)
+    {
+        if (!btn_isLeaf(node))
+        {
+            result = _max(_btn_height(node->left, height + 1),
+                          _btn_height(node->right, height + 1));
+        }
+    }
+
+    return result;
+}
+
+int btn_height(btn *node)
+{
+    return _btn_height(node, 0);
+}
+
+void btn_inorder(btn *node, void btn_do(btn *, void *), void *ctx)
+{
+    if (!node)
+        return;
+
+    btn_inorder(node->left, btn_do, ctx);
+    btn_do(node, ctx);
+    btn_inorder(node->right, btn_do, ctx);
+}
+
+void btn_postorder(btn *node, void btn_do(btn *, void *), void *ctx)
+{
+    if (!node)
+        return;
+
+    btn_postorder(node->left, btn_do, ctx);
+    btn_postorder(node->right, btn_do, ctx);
+    btn_do(node, ctx);
+}
+
+void btn_preorder(btn *node, void btn_do(btn *, void *), void *ctx)
+{
+    if (!node)
+        return;
+
+    btn_do(node, ctx);
+    btn_preorder(node->left, btn_do, ctx);
+    btn_preorder(node->right, btn_do, ctx);
+}
+
+int _btn_print(btn *tree, int is_left, int offset, int depth, char s[20][255], void toStr(btn *, char *))
+{
+    char b[200];
+    int width = 5;
+
+    if (!tree)
+        return 0;
+    toStr(tree, b);
+    // sprintf(b, "%s", str);
+
+    int left = _btn_print(tree->left, 1, offset, depth + 1, s, toStr);
+    int right = _btn_print(tree->right, 0, offset + left + width, depth + 1, s, toStr);
+
+    // for (int i = 0; i < width; i++) s[2 * depth][offset + left + i] = b[i];
+    for (int i = 0; i < strlen(b); i++)
+        s[2 * depth][offset + left + i] = b[i];
+
+    if (depth && is_left)
+    {
+        for (int i = 0; i < width + right; i++)
+            s[2 * depth - 1][offset + left + width / 2 + i] = '-';
+
+        s[2 * depth - 1][offset + left + width / 2] = '+';
+        s[2 * depth - 1][offset + left + width + right + width / 2] = '+';
+    }
+    else if (depth && !is_left)
+    {
+        for (int i = 0; i < left + width; i++)
+            s[2 * depth - 1][offset - width / 2 + i] = '-';
+
+        s[2 * depth - 1][offset + left + width / 2] = '+';
+        s[2 * depth - 1][offset - width / 2 - 1] = '+';
+    }
+
+    return left + width + right;
+}
+
+void btn_print(btn *tree, void toStr(btn *, char *))
+{
+    char s[20][255];
+    for (int i = 0; i < 20; i++)
+        sprintf(s[i], "%80s", " ");
+
+    _btn_print(tree, 0, 0, 0, s, toStr);
+
+    for (int i = 0; i < 20; i++)
+    {
+        int j = 0;
+        while (s[i][j] != '\0' && s[i][j] == ' ')
+        {
+            j++;
+        }
+        if (s[i][j] != '\0')
+        {
+            printf("%s\n", s[i]);
+        }
+    }
+}
---
+++ TP6/btree_base.h
@@ -0,0 +1,132 @@
+/**************************************************************/
+/*                       NOMENCLATURAS                        */
+/**************************************************************/
+/**
+ * BT - Binary Tree: Árbol binario.
+ * BTN - binary tree node: nodo de árbol binario.
+ * SBT - Search Binary Tree: ABB - Árbol binario de búsqueda.
+ * AVL - AVL Tree: Árbol AVL.
+ * root: nodo raíz del árbol.
+ * parent node: nodo padre.
+ * child node: nodo hijo.
+ * internal node,
+ * inner node,
+ * inode,
+ * branch node: nodo interno, que tiene hijos.
+ * external node,
+ * leaf node,
+ * outer node,
+ * terminal node: nodo hoja de un árbol.
+ */
+/**************************************************************/
+
+/**
+ *  Binary Tree Node Struct
+ */
+#define t_elem_btree int
+
+typedef struct _btn
+{
+    t_elem_btree value;
+    struct _btn *left;
+    struct _btn *right;
+} btn;
+
+/**************************************************************/
+/*                     ÁRBOLES BINARIOS                       */
+/**************************************************************/
+
+/**
+ * Crea un nodo de un BTN en base a un valor t_elem_btree
+ */
+btn *btn_new(t_elem_btree value);
+
+/**
+ * Elimina un nodo, si tiene hijos elimina sus hijos también.
+ */
+void btn_free(btn **node);
+
+void btn_free_helper(btn *node, void *ctx);
+
+/**
+ * Busca un nodo con un determinado elemento y devuelve su referencia
+ * Se busca el nodo en pre-order (debe recorrer todo el árbol no hay un criterio de ordenamiento)
+ * Debe devolver la referencia a la ubicación del puntero al nodo. Si no existe, devuelve NULL
+ */
+btn **btn_find(btn **node, t_elem_btree value, int cmp(t_elem_btree, t_elem_btree));
+
+/**
+ * Devuelve 1 si el nodo es una hoja.
+ * Devuelve 0 si no es una hoja.
+ */
+int btn_isLeaf(btn *node);
+
+/**
+ * Cuenta la cantidad de nodos de un árbol binario.
+ */
+int btn_count(btn *node);
+
+/**
+ * Agrega un nodo en un árbol binario con el siguiente criterio:
+ * - Si el subárbol es nulo se agrega ahí,
+ * - Si no es NULL lo agrega en el hijo con menor cantidad de nodos,
+ * - Si sus hijos tienen la misma cantidad de nodos lo agrega a la izquierda.
+ */
+int btn_insert(btn **node, btn *newNode);
+
+/**
+ * Agrega un valor a un BT con el criterio:
+ * - Si el subárbol es nulo se agrega ahí,
+ * - Si no es NULL lo agrega en el hijo con menor cantidad de nodos,
+ * - Si sus hijos tienen la misma cantidad de nodos lo agrega a la izquierda.
+ */
+int btn_insert_value(btn **node, t_elem_btree value);
+
+/**
+ * Determinar el nivel de un nodo.
+ * - Recibe como entrada la raíz, un valor y una función de comparación de valores.
+ */
+int _btn_level(btn *node, t_elem_btree value, int level, int cmp(t_elem_btree, t_elem_btree));
+
+int btn_level(btn *root, t_elem_btree value, int cmp(t_elem_btree, t_elem_btree));
+
+/**
+ * Devuelve el valor máximo entre 2 enteros.
+ * Función auxiliar utilizada en height.
+ */
+//int _max(int a, int b);
+
+/**
+ * Devuelve la altura de un BTN
+ */
+t_elem_btree btn_height(btn *node);
+
+/**
+ * Recorrido de un BT en inorden
+ */
+void btn_inorder(btn *node, void btn_do(btn *, void *), void *ctx);
+
+/**
+ * Recorrido de un BT en postorder
+ */
+void btn_postorder(btn *node, void btn_do(btn *, void *), void *ctx);
+
+/**
+ * Recorrido de un BT en preorder
+ */
+void btn_preorder(btn *node, void btn_do(btn *, void *), void *ctx);
+
+/**************************************************************/
+/*                         ADICIONALES                        */
+/**************************************************************/
+
+/**
+ * DE USO INTERNO para dibujar un árbol.
+ */
+int _btn_print(btn *tree, int is_left, int offset, int depth, char s[20][255], void toStr(btn *, char *));
+
+/**
+ * Dibuja un árbol binario con caracteres
+ * (Los valores de los nodos deben estar entre 0 y 999).
+ */
+void btn_print(btn *tree, void toStr(btn *, char *));
