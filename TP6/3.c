e4a4c713a232800c94e0cb6f2d273747790ba813
Nazareno Montesoro <n.montesoro@gmail.com>
2022-05-26 15:25:07 -0300

    Initial commit

---
+++ TP6/3.c
@@ -0,0 +1,58 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "libs/btree_base.h"
+
+/*
+
+     15
+ 5        3
+6 9     7  10
+
+ */
+
+btn *create_tree();
+void tree_sum_helper(btn *node, void *ctx);
+int tree_sum(btn *root);
+
+btn *create_tree()
+{
+    int arr[] = {5, 3, 6, 7, 9, 10};
+    btn *root = btn_new(15);
+
+    for (int i = 0; i < 6; i++)
+    {
+        btn_insert_value(&root, arr[i]);
+    }
+
+    return root;
+}
+
+void tree_sum_helper(btn *node, void *ctx)
+{
+    *(int *)ctx += node->value;
+}
+
+int tree_sum(btn *root)
+{
+    int result = 0;
+
+    btn_postorder(root, tree_sum_helper, (void *)&result);
+
+    return result;
+}
+
+int main(int argc, char const *argv[])
+{
+    int sum = 0;
+    const int expected_sum = 15 + 5 + 3 + 6 + 7 + 9 + 10;
+
+    btn *root = create_tree();
+
+    sum = tree_sum(root);
+
+    printf("La suma fue %scorrecta.\n", sum == expected_sum ? "" : "in");
+
+    btn_free(&root);
+
+    return 0;
+}
